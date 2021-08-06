39448ce84b72eadfcda849bdbfb365974051b44c
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-07 05:28:33 +0700

    slicing login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,24 @@
 import React, { useEffect } from "react";
-import Contatcs from "../../components/base/contacts/";
-
+import { Link } from "react-router-dom";
+import LayoutAuth from "../../components/module/LayoutAuth";
 function Index() {
   useEffect(() => {
     document.title = "Tambah Produk Untuk Dijual";
   });
   return (
     <>
-      <Contatcs />
+      <LayoutAuth>
+        <form>
+          <div className="d-flex flex-column">
+            <input></input>
+            <input></input>
+            <Link className="c-grey align-self-end" to="/forgot-password">
+              Forgot password?
+            </Link>
+            <button>Login</button>
+          </div>
+        </form>
+      </LayoutAuth>
     </>
   );
 }
