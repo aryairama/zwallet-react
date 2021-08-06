9ed58f9be1501db57e8fd1262ef19dd7424b158a
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-07 05:24:03 +0700

    import and export assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import imgAuth from "./img/image/imgAuth.png";
+import phoneSplit from "./img/image/phoneSplit.png";
+import { default as reset } from "./css/reset.css";
+import { default as typography } from "./css/typography.css";
+import { default as auth } from "./css/auth.css";
+import { default as color } from "./css/color.css";
+
+export { imgAuth, phoneSplit, typography, color, reset, auth };
