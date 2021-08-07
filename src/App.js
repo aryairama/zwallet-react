c513b1e07407e03014e7649c22aa8c0ca4681e42
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-07 22:12:25 +0700

    new route to forgot password

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Login from "./pages/login";
 import Register from "./pages/register";
-
+import ForgotPassword from "./pages/forgotpassword";
 import { Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <Fragment>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/forgot-password" component={ForgotPassword} />
     </Fragment>
   );
 }
