14671226f764d9831b584a4257e7b36fe2808e19
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-07 11:46:58 +0700

    component input  password

---
+++ src/components/base/Input/InputPasswordIcon.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import cs from 'classnames';
+import style from './InputPasswordIcon.module.css';
+
+const InputPasswordIcon = (props) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  return (
+    <React.Fragment>
+      <div className={`position-relative mb-3 ${props.styleContainer}`}>
+        <input
+          onChange={props.onChange}
+          onFocus={props.onFocus}
+          onBlur={props.onBlur}
+          id={props.id}
+          type={showPassword === false ? 'password' : 'text'}
+          name={props.name}
+          value={props.value}
+          className={`${cs(style.input)} ${props.className}`}
+          placeholder={props.placeholder}
+          min={props.min}
+          max={props.max}
+        />
+        <span className={`${cs(style.labelEffect)} ${props.labelEffect}`}></span>
+        <img
+          className={cs(style.iconInput)}
+          width={props.width}
+          height={props.height}
+          src={props.img}
+          alt="icon-input"
+        />
+        <img
+          onClick={() => setShowPassword(!showPassword)}
+          className={cs(style.iconPassword)}
+          width={props.width}
+          height={props.height}
+          src={props.eyePassword}
+          alt="icon-input"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default InputPasswordIcon;
