f315b64a5cbd4c93128ab6a4da7416a7f71e3372
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-08 17:13:51 +0700

    update eslint

--- src/configs/redux/store.js
+++ src/configs/redux/store.js
@@ -1,16 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducers from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducers from "../reducers/rootReducer";
 
 const persistConfig = {
-  key: 'zwallet',
+  key: "zwallet",
   storage,
-  whitelist: ['user'],
+  whitelist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
-export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export const persistor = persistStore(store);
