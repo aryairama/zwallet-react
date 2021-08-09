de18a6d8650bb8aeaf670002c6bf1ed07f3e5619
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-08-09 09:15:50 +0700

    fixing formatting

--- src/pages/createPin/index.js
+++ src/pages/createPin/index.js
@@ -1,43 +1,44 @@
-import React from 'react'
-import Style from './pin.module.css'
-import LayoutAuth from '../../components/module/LayoutAuth'
-import { Button } from '../../components/base'
+import React from 'react';
+import Style from './pin.module.css';
+import LayoutAuth from '../../components/module/LayoutAuth';
+import { Button } from '../../components/base';
 const CreatePin = () => {
-    // const [pin, setPin] = useState('')
-    // const handleChange = (e) => {
-    //     setPin
-    // }
-    return (
+  // const [pin, setPin] = useState('')
+  // const handleChange = (e) => {
+  //     setPin
+  // }
+  return (
+    <div>
+      <LayoutAuth>
+        <p className="text-24">
+          Secure Your Account, Your Wallet, and Your Data With 6 Digits PIN That
+          You Created Yourself.
+        </p>
         <div>
-            <LayoutAuth>
-                <p className="text-24">
-                    Secure Your Account, Your Wallet,
-                    and Your Data With 6 Digits PIN
-                    That You Created Yourself.
-                </p>
-                <div>
-                    <p className="text-16 c-grey" style={{ opacity: "60%" }}>
-                        Create 6 digits pin to secure all your money and your data in Zwallet app. Keep it secret and don’t tell anyone about your Zwallet account password and the PIN.
-                    </p>
-                </div>
-                <div className={Style.pinContainer}>
-                    <input type="text" className={Style.pin} />
-                    <input type="text" className={Style.pin} />
-                    <input type="text" className={Style.pin} />
-                    <input type="text" className={Style.pin} />
-                    <input type="text" className={Style.pin} />
-                    <input type="text" className={Style.pin} />
-                </div>
-                <Button
-                    disabled
-                    styling="bg__primary text-18 c-white"
-                    style={{ marginTop: "90px", width: '100%' }}
-                >
-                    Confirm
-                </Button>
-            </LayoutAuth>
+          <p className="text-16 c-grey" style={{ opacity: '60%' }}>
+            Create 6 digits pin to secure all your money and your data in
+            Zwallet app. Keep it secret and don’t tell anyone about your Zwallet
+            account password and the PIN.
+          </p>
         </div>
-    )
-}
+        <div className={Style.pinContainer}>
+          <input type="text" className={Style.pin} />
+          <input type="text" className={Style.pin} />
+          <input type="text" className={Style.pin} />
+          <input type="text" className={Style.pin} />
+          <input type="text" className={Style.pin} />
+          <input type="text" className={Style.pin} />
+        </div>
+        <Button
+          disabled
+          styling="bg__primary text-18 c-white"
+          style={{ marginTop: '90px', width: '100%' }}
+        >
+          Confirm
+        </Button>
+      </LayoutAuth>
+    </div>
+  );
+};
 
-export default CreatePin
+export default CreatePin;
--- src/pages/managePhoneNumber/index.js
+++ src/pages/managePhoneNumber/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import '../../assets/css/color.css'
-import '../../assets/css/typography.css'
-import Card from '../../components/base/card'
+import React from 'react';
+import '../../assets/css/color.css';
+import '../../assets/css/typography.css';
+import Card from '../../components/base/card';
 const ManagePhone = () => {
-    return (
-        <>
-            <div className='wrapperContent'>
-                <p className='text-18 bold'>Personal Information</p>
-                <p className='text-16 c-dark desciptionContentBox'>We got your personal information from the sign up proccess. If you want to make changes on your information, contact our support.</p>
-                <Card type='managePhone' title='Primary' content='+62 813-9387-7946'/>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="wrapperContent">
+        <p className="text-18 bold">Personal Information</p>
+        <p className="text-16 c-dark desciptionContentBox">
+          We got your personal information from the sign up proccess. If you
+          want to make changes on your information, contact our support.
+        </p>
+        <Card type="managePhone" title="Primary" content="+62 813-9387-7946" />
+      </div>
+    </>
+  );
+};
 
-export default ManagePhone
+export default ManagePhone;
