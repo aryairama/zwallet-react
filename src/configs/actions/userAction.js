926c2ea8607b63d901302217b1c45ae7a7d21661
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-09 19:21:32 +0700

    forgot password

--- src/configs/actions/userAction.js
+++ src/configs/actions/userAction.js
@@ -10,49 +10,30 @@ export const register = (data, history) => async (dispatch) => {
       email: data.email,
       password: data.password,
     });
-    swal(
-      'Success',
-      'Register successful, check your email to activation',
-      'success'
-    );
+    swal('Success', 'Register successful, check your email to activation', 'success');
     history.push('/login');
   } catch (error) {
     if (error?.response?.data?.statusCode === 422) {
-      swal(
-        'Failed',
-        error?.response?.data?.error[0]?.msg || 'Back end mati',
-        'error'
-      );
+      swal('Failed', error?.response?.data?.error[0]?.msg || 'Back end mati', 'error');
     } else {
-      swal(
-        'Failed',
-        error?.response?.data?.message || 'Back end mati',
-        'error'
-      );
+      swal('Failed', error?.response?.data?.message || 'Back end mati', 'error');
     }
   }
   dispatch({ type: 'REQUEST' });
 };
 
-export const checkEmail = (data, history) => async (dispatch) => {
+export const checkEmail = async (email) => {
   try {
-    await axios.post('/users/forgotPassword', {
-      email: data.email,
-    });
-    swal('Success', 'check email to create a new password', 'success');
+    await axios.post('/users/forgotPassword', { email });
+    swal('Success', 'Check your email to continue changing password', 'success');
   } catch (error) {
-    swal(
-      'Failed',
-      error?.response?.data?.message || 'Internal Server Error',
-      'error'
-    );
+    swal('Error', 'Email not found', 'error');
   }
-  dispatch({ type: 'REQUEST' });
 };
 
 export const login = (formData, history) => async (dispatch) => {
   try {
-    const { data } = await (await axios.post('users/login', formData)).data;
+    const { data } = await (await axios.post('/users/login', formData)).data;
     dispatch({ type: 'LOGIN', payload: data });
     swal('Success', 'Login success', 'success');
     history.push('/dashboard');
@@ -69,7 +50,7 @@ export const login = (formData, history) => async (dispatch) => {
 
 export const logout = (history) => async (dispatch, getState) => {
   try {
-    await axios.delete('users/logout', {
+    await axios.delete('/users/logout', {
       headers: {
         Authorization: `Bearer ${getState().user.user.accessToken}`,
       },
@@ -85,4 +66,34 @@ export const refreshToken = (data) => (dispatch, getState) => {
   const { user: oldDataUser } = getState().user;
   const user = { ...oldDataUser, ...data };
   dispatch({ type: 'REFRESHTOKEN', payload: user });
+};
+
+export const checkTokenResetPassword = async (token, history) => {
+  try {
+    const { data } = await (await axios.get(`/users/forgotpassword/${token}`)).data;
+    return data;
+  } catch (error) {
+    swal('Error', 'Your session is invalid', 'error').then((res) => {
+      if (res === true) {
+        history.push('/login');
+      }
+    });
+  }
+};
+
+export const resetPassword = async (formData, id, history) => {
+  try {
+    axios.post('/users/changepassword', {
+      password: formData.newpassword,
+      password2: formData.verifnewpasword,
+      id: id,
+    });
+    swal('Success', 'Password has been changed successfully, please login.', 'success').then((res) => {
+      if (res) {
+        history.push('/login');
+      }
+    });
+  } catch (error) {
+    swal('Error', 'Password failed to change', 'error');
+  }
 };
