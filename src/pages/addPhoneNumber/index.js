2ee97b0634922987335b313315f3733a18904744
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-08-10 20:11:04 +0700

    adding page add phone number

--- src/pages/addPhoneNumber/index.js
+++ src/pages/addPhoneNumber/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import { InputTextIcon } from '../../components/base';
+import Phone from '../../assets/img/icons/phoneInput.svg';
+import Style from './phone.module.css';
+import { Button } from '../../components/base';
+import SimpleReactValidator from 'simple-react-validator';
 const AddPhoneNumber = () => {
+  const validator = React.useRef(new SimpleReactValidator({ className: 'small text-danger' }));
   return (
     <>
       <div className="wrapperContent">
@@ -8,6 +13,16 @@ const AddPhoneNumber = () => {
         <p className="text_16 c-dark desciptionContentBox">
           Add at least one phone number for the transfer ID so you can start transfering your money to another user.
         </p>
+        <form className={Style.phoneWrapper}>
+          <InputTextIcon img={Phone} width="21px" height="21px" placeholder="Enter your phone number" name="phone" />
+          <Button
+            disabled={validator.current.allValid() ? false : true}
+            styling="bg__primary text-18 c-white"
+            style={{ marginTop: '40px', marginBottom: '40px' }}
+          >
+            Change Phone Number
+          </Button>
+        </form>
       </div>
     </>
   );
