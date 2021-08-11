608a1672cb9d609257a3872ff4c423469a7354a7
Alan Nugraha <101muhammadarifin@gmail.com>
2021-08-11 20:48:36 +0700

    pull from staging

--- src/pages/statusTransfer/index.jsx
+++ src/pages/statusTransfer/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import CardContainer from '../../components/base/cardContainer';
 import { success } from '../../assets';
