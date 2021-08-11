bad991dacc04cddc15ee07fab3a1617da222b64d
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-08-11 14:54:26 +0700

    fixing pagination

--- src/pages/transactionHistory/index.jsx
+++ src/pages/transactionHistory/index.jsx
@@ -15,10 +15,10 @@ function Index() {
   const dispatch = useDispatch();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(async () => {
-    await dispatch(getTransaction(5, 'DESC', 'ALL_USER', page, 'created_at'));
+    await dispatch(getTransaction(2, 'DESC', page, 'created_at'));
   }, [page]);
   const { transactionList } = useSelector((state) => state.transaction);
-  const { user } = useSelector((state) => state.user)
+  const { user } = useSelector((state) => state.user);
   function convertToRupiah(angka) {
     var rupiah = '';
     var angkarev = angka.toString().split('').reverse().join('');
@@ -44,32 +44,36 @@ function Index() {
       <CardContainer className="bg__white">
         <p className="text-bold text-18">Transaction History</p>
         <div>
-        {transactionList?.data ? (
+          {transactionList?.data ? (
             transactionList?.data?.map((transaction, index) => (
-            <Link to={`/status-transfer/${transaction.transaction_id}`}>
-              <Card
-                key={index}
-                type="transactionList"
-                image={
-                  transaction.transaction_type === 'topup' ? `${process.env.REACT_APP_API_URL}/${user.image}` : transaction.image_reciever ? `${process.env.REACT_APP_API_URL}/${transaction.image_reciever}` : Avatar
-                }
-                name={transaction.transaction_type === 'topup' ? `${transaction.fullname}`:`${transaction.recipient}`}
-                transaction_type={transaction.transaction_type}
-                transactionVal={transaction.transaction_type === 'topup' ? true : false}
-                amount={convertToRupiah(convertToAngka(transaction.amount))}
-              />
-            </Link>
-          ))) : 
-          (
+              <Link to={`/status-transfer/${transaction.transaction_id}`} key={index}>
+                <Card
+                  type="transactionList"
+                  image={
+                    transaction.transaction_type === 'topup'
+                      ? `${process.env.REACT_APP_API_URL}/${user.image}`
+                      : transaction.image_reciever
+                      ? `${process.env.REACT_APP_API_URL}/${transaction.image_reciever}`
+                      : Avatar
+                  }
+                  name={
+                    transaction.transaction_type === 'topup' ? `${transaction.fullname}` : `${transaction.recipient}`
+                  }
+                  transaction_type={transaction.transaction_type}
+                  transactionVal={transaction.transaction_type === 'topup' ? true : false}
+                  amount={convertToRupiah(convertToAngka(transaction.amount))}
+                />
+              </Link>
+            ))
+          ) : (
             <p className="text-18">You dont have any transaction</p>
-          )
-          }
+          )}
         </div>
         <div className="row">
           <div className="col-12">
             {transactionList?.pagination && (
               <Pagination
-                current={page > 0 ? page - 1 : page}
+                current={page}
                 total={transactionList.pagination.countData}
                 pageSize={transactionList.pagination.limit}
                 itemRender={buttonItemRender}
@@ -79,39 +83,6 @@ function Index() {
             )}
           </div>
         </div>
-        {/* <div>
-          <p className="text-16 c-greySLight">This Week</p>
-          {transactionList?.data.map((receiver, index) => (
-            <Link to={`/input-amount/${receiver.user_id}`}>
-              {user.user_id !== receiver.user_id && (
-                <Card
-                  key={index}
-                  type="tfHistory"
-                  image={receiver.image ? `${process.env.REACT_APP_API_URL}/${receiver.image}` : Avatar}
-                  name={`${receiver.first_name} ${receiver.last_name}`}
-                  phone={receiver.phone_number}
-                />
-              )}
-            </Link>
-          ))}
-        </div>
-        <div className="mt-4">
-          <p className="text-16 c-greySLight">This Month</p>
-          {transactionList?.data.map((receiver, index) => (
-            <Link to={`/input-amount/${receiver.user_id}`}>
-              {user.user_id !== receiver.user_id && (
-                <Card
-                  key={index}
-                  type="tfHistory"
-                  image={receiver.image ? `${process.env.REACT_APP_API_URL}/${receiver.image}` : Avatar}
-                  name={`${receiver.first_name} ${receiver.last_name}`}
-                  phone={receiver.phone_number}
-                />
-              )}
-            </Link>
-          ))}
-        </div>
-         */}
       </CardContainer>
     </React.Fragment>
   );
